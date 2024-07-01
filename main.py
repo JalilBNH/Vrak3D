f82b511f4dafd5d291894f095a4fa6606234157f
JalilBNH <jalil.bnh13@gmail.com>
2024-07-01 13:22:02 +0200

    main

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from utils import computeMeanSlope, computeMeanDistance, visualizePolygons
+from utils import computeMeanSlope, computeMeanDistance, visualizePolygons, meanPolygon
 
 def main():
     polygons = [[(1,1), (2,3), (3,1)], [(1,1), (2,10), (3,1)], [(1,1), (2,2), (3,-1)]]
     
-    visualizePolygons(polygons)
-    
-    print(computeMeanSlope(polygons))
-    print(computeMeanDistance(polygons))
+    print(meanPolygon(polygons))
 
 if __name__ == '__main__':
     main()
